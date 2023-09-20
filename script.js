116ecd0c90459b10fcfda4bf0f3f2e1ee5cbb88e
estherpinos <estherpinos1@gmail.com>
2023-09-20 16:45:27 +0200

    Edit [numeri random and reset numeri schermo]

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
-let numeri = document.getElementsByClassName('number');
+let numeri = document.getElementsByClassName('.number');
 let play = document.getElementById('btn');
 
+// 1.Creo array vuoto dove mettere i numeri random.
 
-const listaNumeri=[]
-let numeritotai=5;
+let numeriTotali=5;
 
+// 2.Cuando faccio click sul bottone:
 document.querySelector('button').addEventListener('click', function()
 {
-  generaNumeri();
+ reset();
+ let listaNumeri= generaNumeri();
+ mostraNumeri(listaNumeri);
+ setTimeout(function(){
+  reset();
+ },5000);
+  
 
 })
 
+// FUNZIONI
+
 function generaNumeri(){
-  let numeriRandom= Math.ceil(Math.random() * 100);
-while (listaNumeri<numeritotai) {
- let numeroRandom = Math.ceil(Math.random() * 100);
- listaNumeri.push(numeroRandom);
- console.log(listaNumeri);
- 
-}
-return listaNumeri;
+
+  let listaNumeri=[];
+  for (let i = 0; i < numeriTotali; i++) {
+    let numeroRandom = Math.ceil(Math.random() * 100);
+    listaNumeri.push(numeroRandom);
+  }
+  return listaNumeri;
 
 }
+
+function mostraNumeri(listaNumeri){
+  for (let i = 0; i < listaNumeri.length; i++) {
+    const element = listaNumeri[i];
+    document.getElementById('numeri').innerHTML+=`<h2> ${element} </h2>`;
+    
+  }
+
+}
+
+function reset(){
+  document.getElementById('numeri').innerHTML=``;
+}
+
+
+
+
+
+
+
+
+
+
